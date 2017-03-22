c1579835abc15c43fc04331b37203d03e34351bb
Rhanyel Rodrigues <rhanyskate@hotmail.com>
2017-03-22 14:20:06 -0300

    loginauth, dashboard and bootstrap added

--- core/models.py
+++ core/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Customer(models.Model):
+	name = models.CharField(max_length=100)
+	nick = models.CharField(max_length=10)
+	mail = models.CharField(max_length = 30)
+	password = models.CharField(max_length = 16)
+	def __unicode__(self):
+		return self.name
+
+
 # Create your models here.
--- core/views.py
+++ core/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Customer
+from django.http import HttpResponseRedirect
+from core.forms import CustomerForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+
+username = 'null'
 
 # Create your views here.
+"""
+def login(request):
+    customer_list = Customer.objects.all
+    form = CustomerForm()
+    context_alter = {'customers': customer_list}
+    if request.method == "POST":
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            c = Customer()
+            c.name = form.cleaned_data["name"]
+            c.save()
+            return redirect("/dashboard/")
+        else:
+            form = CustomerForm()
+    context_dict = {'customers': customer_list, 'form': form}
+    return render(request, 'index.html', context_dict)"""
+
+def logar(request):
+        if request.method == 'POST':
+            form = AuthenticationForm(data=request.POST)
+            if form.is_valid():
+                login(request, form.get_user())
+                username = request.POST['username']
+                return redirect("/dashboard/")
+            else:
+                return render(request, 'index.html', {"form": form})
+        return render(request, 'index.html', {"form": AuthenticationForm()})
+
+   
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
